b85a5db15fa77ae958d8ffb47007e54cc8708d08
Guilherme Domith <guilhermedomith@gmail.com>
2018-11-12 14:06:16 -0200

    Comunicação Bluetooth adicionado.

--- Servidor_Comandos/comandos.py
+++ Servidor_Comandos/comandos.py
@@ -1,18 +1,37 @@
 """
     Módulo para comunicação com o arduíno via bluetooth.
 """
+import bluetooth
+import sys
 
-def led(status=False):
-    pass
+class ComandosResidencia():
 
-def alarme(status=False, sensibilidade=100):
-    pass
+    def __init__(self, bt_addr): 
+        self.port = 1
+        self.bt_addr = bt_addr
 
-def ventilador(status=False, potencia=100):
-    pass
+        self.sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+        self.sock.connect((bt_addr, self.port))
+        self.sock.settimeout(1.0)
+        self.status = 'Conectado'
 
-comandos = {
-    'led': led,
-    'alarme': alarme,
-    'ventilador': ventilador
-}
+        self.comandos = {
+            'led': self.led,
+            'alarme': self.alarme,
+            'ventilador': self.ventilador
+        }
+
+    def led(self, status=False):
+        self.sock.send("led=%s"%status)
+
+    def alarme(self, status=False, sensibilidade=100):
+        self.sock.send("alarme=%s"%status)
+
+    def ventilador(self, status=False, potencia=100):
+        self.sock.send("ventilador=%s"%status)
+
+    def executa(self, method, args=()):
+        self.comandos[method](args)
+
+    def encerrar_conexao(self):
+        self.sock.close()
--- Servidor_Comandos/servidor.py
+++ Servidor_Comandos/servidor.py
@@ -1,10 +1,27 @@
 from flask import Flask, request
-from comandos import comandos
+from comandos import ComandosResidencia
 import json
 
 app = Flask(__name__)
+residencia = ''
+# https://stackoverflow.com/questions/15486570/bluetooth-communication-between-arduino-and-pybluez
 
-@app.route('/', method=['GET'])
+@app.route('/', methods=['GET'])
 def comando_residencia():
     RPC = json.dumps(request.form['RPC'])
-    comandos[RPC['method']](RPC['args'])
+    residencia.executa(RPC['method'], RPC['args'])
+
+    return ''
+
+
+@app.route('/<method>/<int:status>', methods=['GET'])
+def teste(method, status):
+    status = bool(status)
+
+    residencia.executa(method, status)
+    return json.dumps({method: status})
+
+
+if __name__ == '__main__':
+    residencia = ComandosResidencia(bt_addr="00:12:10:23:10:18")
+    app.run(debug=True, port=5000)
