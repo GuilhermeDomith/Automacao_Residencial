6dbf073e907102bd3ef1405933d2c2052bbf3b7c
Guilherme <guilhermekaioken@gmail.com>
2018-11-13 19:45:58 -0200

    [ADD] adicionando o teste de sensor de proximidade e buzzer

---
+++ Arduino_Residencia/TesteSensorProximidadeEBuzzer.ino
@@ -0,0 +1,42 @@
+//Programa: Conectando Sensor Ultrassonico HC-SR04 ao Arduino
+//Autor: FILIPEFLOP
+#define ALARME 8
+#define DISTANCIA 8.50
+//Carrega a biblioteca do sensor ultrassonico
+#include <Ultrasonic.h>
+ 
+//Define os pinos para o trigger e echo
+#define pino_trigger 4
+#define pino_echo 5
+ 
+//Inicializa o sensor nos pinos definidos acima
+Ultrasonic ultrasonic(pino_trigger, pino_echo);
+ 
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Lendo dados do sensor...");
+
+  pinMode(ALARME,OUTPUT);
+}
+ 
+void loop()
+{
+  //Le as informacoes do sensor, em cm e pol
+  float cmMsec, inMsec;
+  long microsec = ultrasonic.timing();
+  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  inMsec = ultrasonic.convert(microsec, Ultrasonic::IN);
+
+  if(cmMsec <= DISTANCIA){
+    digitalWrite(ALARME,HIGH);
+    //delay(10000);
+  }else
+    digitalWrite(ALARME,LOW);
+  //Exibe informacoes no serial monitor
+  Serial.print("Distancia em cm: ");
+  Serial.print(cmMsec);
+  Serial.print(" - Distancia em polegadas: ");
+  Serial.println(inMsec);
+  
+}
