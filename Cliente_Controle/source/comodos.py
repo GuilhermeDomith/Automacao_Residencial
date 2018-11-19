e9e3c15ddebacff46fae2ce57fc3ac1bfe79d4d0
Lucas Heber <lucas.heber07@gmailcom>
2018-11-19 20:32:27 -0200

    [ADD] App web

---
+++ Cliente_Controle/source/comodos.py
@@ -0,0 +1,15 @@
+class SalaEstar():
+    _name = 'Sala de Estar'
+
+    components = [
+        {'pin': 13, 'status': 0, 'description': 'Lustre'},
+        {'pin': 12, 'status': 0, 'description': 'Abajur'}
+    ]
+
+class Cozinha():
+    _name = 'Cozinha'
+
+    components = [
+        {'pin': 13, 'status': 0, 'description': 'Fogão'},
+        {'pin': 12, 'status': 0, 'description': 'Exaustor'}
+    ]
