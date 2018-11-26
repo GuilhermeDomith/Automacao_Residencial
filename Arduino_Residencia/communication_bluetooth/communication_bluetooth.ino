84b8ec5acfe994e646a2cf29e2fdfb9d4252633b
José <josedemelo.if@gmail.com>
2018-11-26 21:22:57 -0200

    [UP] Adicionando novas funções ao código do Arduíno

--- Arduino_Residencia/communication_bluetooth/communication_bluetooth.ino
+++ Arduino_Residencia/communication_bluetooth/communication_bluetooth.ino
@@ -1,23 +1,34 @@
 #include <SoftwareSerial.h>
 #include <string.h>
+#include <Ultrasonic.h>
+ 
 
+//Define os pinos para o trigger e echo
+#define pino_trigger 3
+#define pino_echo 2
+
+#define ALARME 6
+#define DISTANCIA 8.50
 #define COMP_LED 1
 #define COMP_VEN 2
 
-int LED = 13;
 bool state = false;
+bool alarmeAtivado = false;
+bool movimentoDetectado = false;
 
 int statusLeds[] = {0, 0, 0, 0};
-int pinosLeds[] = {13, 14, 15, 16};
+int pinosLeds[] = {13, 12, 8, 7};
 char codigo[3];
 bool requisicaoRecebida = false;
  
 SoftwareSerial serial1(11,10);
+Ultrasonic ultrasonic(pino_trigger, pino_echo);
 
 void setup() {
   serial1.begin(9600);
   Serial.begin(9600);
-  pinMode(LED, OUTPUT);
+  definirPinosComoSaida();
+  pinMode(ALARME,OUTPUT);
 }
 
 void loop() {
@@ -37,31 +48,36 @@ void loop() {
   if(codigo[0] != '0'){
     verificarCodigo();
   }
+  
   controlarLeds();
+  alarme();
   delay(2000);
 
-}
-
-void executarAcao(){
-  if(codigo[0] == 'l' || codigo[0] == 'L'){
-     
-  }
-
-  
 }
 
 void verificarCodigo(){
   String mensagem = "", numero;
 
-  if(codigo[2] == '1'){
-    mensagem.concat("Acender/ligar o ");
-    alterarStatusLed(0, true); 
-  }else{
-    alterarStatusLed(0, false);
+  if(codigo[0] == 'L' || codigo[0] == 'l'){
+    String indiceString;
+    indiceString.concat(codigo[1]);
+    int indice = indiceString.toInt();
+
+    if(codigo[2] == '1'){
+      alterarStatusLed(indice, true); 
+    }else{
+      alterarStatusLed(indice, false);
+    }    
   }
 
-  if(codigo[0] == 'L' || codigo[0] == 'l'){
-    mensagem.concat("LED "); 
+  else{
+    if(codigo[0] == 'A' || codigo[0] == 'a'){
+      if(codigo[1] == '1'){
+        controlarAlarme(true);
+      }else{
+        controlarAlarme(false);
+      }
+    }
   }
 
   mensagem.concat(codigo[1]);
@@ -73,11 +89,7 @@ void verificarCodigo(){
     Serial.println("Acenda o led.");
   }
 
-  
   Serial.println(mensagem);
-  
-
-  
 }
 
 
@@ -101,4 +113,36 @@ void limpaArrayCodigo(){
     codigo[0] = '0';
     codigo[1] = '0';
     codigo[2] = '0';
+}
+
+void definirPinosComoSaida(){
+  int i = 0;
+  
+  for(i = 0; i < 4; i++) {
+    pinMode(pinosLeds[i], OUTPUT);
+  }
+}
+
+void controlarAlarme(bool ativar){
+    Serial.println("Ativar alarme");
+    alarmeAtivado = ativar;
+}
+void alarme(){
+  float cmMsec, inMsec;
+  long microsec = ultrasonic.timing();
+  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  inMsec = ultrasonic.convert(microsec, Ultrasonic::IN);
+
+  if(alarmeAtivado){
+    if(!movimentoDetectado){
+      if(cmMsec <= DISTANCIA){
+        movimentoDetectado = true;
+      }
+  }else
+    digitalWrite(ALARME,LOW);
+  }  
+
+  if(movimentoDetectado){
+    digitalWrite(ALARME, HIGH);
+  }
 }
