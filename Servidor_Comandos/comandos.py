22be76caa1b9405b0242c61b53ef1a053f5828d9
Guilherme Magnus <guilhermekaioken@gmail.com>
2018-12-03 21:25:02 -0200

    [UP] Implementando busca de status dos componentes

--- Servidor_Comandos/comandos.py
+++ Servidor_Comandos/comandos.py
@@ -5,6 +5,7 @@ import bluetooth
 import sys
 import time
 import socket
+import json
 
 class HomeControl():
 
@@ -30,9 +31,29 @@ class HomeControl():
         self.comandos = {
             'led': self.led,
             'alarme': self.alarme,
-            'modo_automatico': self.modo_automatico
+            'modo_automatico': self.modo_automatico, 
+            'consulta_status_automatico': self.consulta_status_automatico
        }
 
+
+    def consulta_status_automatico(self):
+        self.sock.settimeout(20)
+        self.sock.send('SM')
+
+        data = ''
+    
+        while True:
+            data = self.sock.recv(1024)
+        
+            data = data.decode('utf-8')
+
+            if data:
+                if data != '\r\n':
+                    print("#### => {}".format(data))
+                    break
+
+        return json.dumps({'status': data})
+    
 
     def led(self, id=0, status=0):
         self.sock.send('L%s%s'%(id, status))
@@ -41,17 +62,17 @@ class HomeControl():
         self.sock.send('A%s'%status)
 
         while True:
-            data = self.sock.recv(1024)
+            data = self.sock.recv(2048)
 
             if data:
-                print("#### => {}".format(data))
+                print("#### => {}".format(data.decode('utf-8')))
                 break
 
     def modo_automatico(self, status=0):
         self.sock.send('M%s'%status)
 
     def executa(self, method, params):
-        self.comandos[method](*params)
+        return self.comandos[method](*params)
 
     def encerrar_conexao(self):
         self.sock.close()
