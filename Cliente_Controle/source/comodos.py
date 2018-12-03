31fe53a6be12e33ccf433af601102070791ff734
Lucas Heber <lucas.heber07@gmailcom>
2018-12-03 17:09:04 -0200

    [UP] front page

--- Cliente_Controle/source/comodos.py
+++ Cliente_Controle/source/comodos.py
@@ -1,15 +1,37 @@
 class SalaEstar():
     _name = 'Sala de Estar'
+    _image = 'images/quarto.jpg'
 
     components = [
-        {'pin': 3, 'status': 0, 'description': 'Lustre'},
-        {'pin': 2, 'status': 0, 'description': 'Abajur'}
+        {'pin': 0, 'status': 0, 'description': 'Lâmpada'}
     ]
 
 class Cozinha():
     _name = 'Cozinha'
+    _image = 'images/kitchen.jpg'
 
     components = [
-        {'pin': 3, 'status': 0, 'description': 'Fogão'},
-        {'pin': 2, 'status': 0, 'description': 'Exaustor'}
+        {'pin': 2, 'status': 0, 'description': 'Lâmpada'}
     ]
+
+class Copa():
+    _name = 'Copa'
+    _image = 'images/kitchen.jpg'
+
+    components = [
+        {'pin': 1, 'status': 0, 'description': 'Lâmpada'}
+    ]
+
+class Quarto():
+    _name = 'Quarto'
+    _image = 'images/kitchen.jpg'
+
+    components = [
+        {'pin': 3, 'status': 0, 'description': 'Lâmpada'}
+    ]
+
+
+# sala L0
+# copa L1
+# cozinha L2
+# quarto L3
