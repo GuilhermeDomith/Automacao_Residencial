6fafc3b4bdd65b2fee403f8d69a7ed62ec3b88b1
José de Melo <josedemelo.if@gmail.com>
2018-12-06 21:16:05 -0200

    [UP] Trocando o sistema de criptografia entre o cliente e o servidor

--- Servidor_Comandos/servidor.py
+++ Servidor_Comandos/servidor.py
@@ -1,30 +1,42 @@
 from flask import Flask, request
 from Crypto.Cipher import AES
 from comandos import HomeControl
+from base64 import b64decode
 import json, config
 
-cripto = AES.new(config.password_cripto)
+passwd_criptografia = 'sistemasdistribuidos2018'
 
 home_control = None
 app = Flask(__name__)
+
+
+def descriptografar(data):
+    #print('data dict ', data, type(data))    
+
+    data_cript = b64decode(data['data'].encode())
+    nonce = b64decode(data['nonce'].encode())
+    
+    #print('data cript', data_cript, type(data_cript))
+    #print('nonce', nonce, type(nonce))
+
+    cripto = AES.new(passwd_criptografia.encode(), AES.MODE_EAX, nonce=nonce)
+    data = cripto.decrypt(data_cript)
+    #print('data ', data, type(data))
+
+    return data
 
 
 @app.route('/', methods=['POST'])
 def comando_home_control():
     start_home_control()
 
-    data_cript = request.data
-    data = cripto.decrypt(data_cript)
-
-    # Remove os caracteres adicionados
-    data_str = data.decode()
-    data_str = data_str.rpartition('#')[0]
-
+    #print(request.form)
+    data_str = descriptografar(request.form)
     data = json.loads(data_str)
-    print('RPC Data: ', data)
+    #print('RPC Data: ', data)
 
     jsonResponse = home_control.executa(data['method'], data['params'])
-    print(jsonResponse)
+    #print(jsonResponse)
     return jsonResponse
 
 def start_home_control():
